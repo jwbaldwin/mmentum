2f18beae1a9f11cd316a578ecaa24ee7cf8ad7e4
James Baldwin <jwbaldwin3@aol.com>
2023-11-04 10:44:51 -0400

    add fly deployment

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Mmentum.MixProject do
     [
       app: :mmentum,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
